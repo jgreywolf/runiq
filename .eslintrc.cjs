aa365236401119ef8016863e1d1bda69eeee86b6
Justin Greywolf <jgreywolf@users.noreply.github.com>
2025-10-29 19:36:22 -0700

    Add parser options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
+    project: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
